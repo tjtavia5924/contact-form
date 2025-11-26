f44ed6cb2d626f0e8cb02cd04a390f9eacc91936
tjtavia5924 <taviathompson@hotmail.co.uk>
2025-11-26 10:32:53 +0000

    in proceess of trying to get tailwindcss acknowledged in vite config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+// import React from "react";
 
-export default function App() {
-  return <div>App</div>;
+// export default function App() {
+//   return <div>App</div>;
+// }
+
+
+import { Button } from "@/components/ui/button"
+
+function App() {
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
+
+export default App
