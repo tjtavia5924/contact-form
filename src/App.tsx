5ad61b321838e53f0adb6b84446a54512a882c4d
Tavia Thompson <tav@tavias-laptop.home>
2025-11-28 17:43:34 +0000

    Installed tailwindcss/vite, put  main.tsx as root in HTML file, edited css, app and tsconfig files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-// import React from "react";
-
-// export default function App() {
-//   return <div>App</div>;
-// }
-
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
